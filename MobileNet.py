086253f7297529a63c43186b1d2441ccfc636758
COE516 <kjy11302@gmail.com>
2020-09-04 13:22:14 +0900

    200904

--- MobileNet.py
+++ MobileNet.py
@@ -1,82 +1,156 @@
-import os
+from os import times_result
+
+import cv2
 from PIL import Image
 import glob
-from tensorflow import keras
-from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
-from keras.layers import Dense, Activation
+from keras.layers import Dense, Activation, Conv2D, Dropout, MaxPooling2D, Flatten
 from keras.utils import to_categorical
 import numpy as np
-import cv2
-
 
 poketmon = ["Squirtle", "Digda", "Dragonite", "Modafi", "Yoongella", "Leesang", "Snorlax", "Charmander", "Purin", "Pikachu"]
 img_path = "D:\\Gongmo\\It_worked\\img\\"
 
-label_list = os.listdir(img_path)
 label = [i for i in range(10)]
 
-x_train = []
-x_label = []
+train_data = []
+train_label = []
 
-y_verfi = []
-y_label = []
+verfi_data = []
+verfi_label = []
 
-z_test = []
-z_label = []
+test_data = []
+test_label = []
+
+width = 200
+height = 200
+
+# 학습, 검증, 테스트 데이터 개수 조절
+train_num = 600
+verfi_num = 200
+test_num = 100
+# --------------------------------- #
+
+verfi_num += train_num
+test_num += verfi_num
+
 for b in label:
-
     directory = img_path + str(b) + "\\" + poketmon[int(b)] + "_pp\\"
     files = glob.glob(directory + "*.jpg")
     for i, f in enumerate(files):
         img = Image.open(f)
         data = np.asarray(img) / 255
-        if i <= 599:
-            x_train.append(np.array(data).flatten())
-            x_label.append(b)
-        elif 599 < i and i <= 799:
-            y_verfi.append(np.array(data).flatten())
-            y_label.append(b)
-        elif i < 900 and i > 799:
-            z_test.append(np.array(data).flatten())
-            z_label.append(b)
+        # rgb_data = cv2.cvtColor(data, cv2.COLOR_GRAY2BGR)
+        if i < train_num:
+            train_data.append(np.array(data))
+            train_label.append(b)
+        elif train_num <= i and i < verfi_num:
+            verfi_data.append(np.array(data))
+            verfi_label.append(b)
+        elif i >= verfi_num and i < test_num :
+            test_data.append(np.array(data))
+            test_label.append(b)
         else:
             break
+files = 0 # files에 trash 값 남아있음
 
-# print(len(x_train))
-x_train = np.array(x_train)
-# print(len(x_label))
-x_label = to_categorical(x_label)
-# print(len(y_verfi))
-y_verfi = np.array(y_verfi)
-# print(len(y_label))
-y_label = to_categorical(y_label)
-# print(len(z_test))
-z_test = np.array(z_test)
-# print(len(z_label))
-z_label = to_categorical(z_label)
+# data -> numpy array, label -> one hot encoding
+train_num = 600; verfi_num = 200; test_num = 100
 
+train_data = np.array(train_data)
+train_data = train_data.reshape((train_num * 10, width, height, 1))
+train_label = to_categorical(train_label)
+
+verfi_data = np.array(verfi_data)
+verfi_data = verfi_data.reshape((verfi_num * 10, width, height, 1))
+verfi_label = to_categorical(verfi_label)
+
+test_data = np.array(test_data)
+test_data = test_data.reshape((test_num * 10, width, height, 1))
+test_label = to_categorical(test_label)
+# ----------------------------------------- #
+
+# Normalization
+'''
+print("mean before normalization:", np.mean(train_data))
+print("std before normalization:", np.std(train_data))
+
+mean = [0, 0, 0]
+std = [0, 0, 0]
+newX_train = np.ones(train_data.shape)
+newX_test = np.ones(test_data.shape)
+# train set에 있는 데이터로만 평균과 표준편차를 구함
+for i in range(3):
+    mean[i] = np.mean(train_data[i, :, :])
+    std[i] = np.std(train_data[i, :, :])
+
+# train과 test셋 모두 정규화 작업
+for i in range(3):
+    newX_train[i, :, :] = train_data[i, :, :] - mean[i]
+    newX_train[i, :, :] = newX_train[i, :, :] / std[i]
+    newX_test[i, :, :] = train_data[i, :, :] - mean[i]
+    newX_test[i, :, :] = newX_test[i, :, :] / std[i]
+
+X_train = newX_train
+X_test = newX_test
+
+print("mean after normalization:", np.mean(X_train))
+print("std after normalization:", np.std(X_train))
+print(X_train.max())
+'''
+# ------------------------------------------#
+
+# 모델 구성
 model = Sequential()
-model.add(Dense(256, activation='relu', input_dim = 400 * 400))
-model.add(Dense(256, activation='relu'))
-model.add(Dense(256))
+# model.add(Dense(256, activation='relu', input_dim = 200 * 200))
+# model.add(Dense(256, activation='relu'))
+# model.add(Dense(256))
+# model.add(Dense(10, activation='softmax'))
+
+#-- layer 1
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same', input_shape=(width, height, 1)))
+model.add(Dropout(0.5))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+model.add(Dropout(0.5))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+#-- layer 2
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+model.add(Dropout(0.5))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+#-- layer 3
+model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+model.add(Dropout(0.5))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+#-- layer 4
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
+
+# -- layer 5
+model.add(Dense(512, activation='relu'))
+
+# -- layer 6
 model.add(Dense(10, activation='softmax'))
 
 # 3. 모델 학습과정 설정하기
-model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy',
+              optimizer='sgd',
+              metrics=['accuracy'])
 
 # 4. 모델 학습시키기
-model.fit(x_train, x_label, epochs=5, batch_size=32, validation_data=(y_verfi, y_label))
+model.fit(train_data, train_label, epochs=5, batch_size=32, validation_data=(verfi_data, verfi_label), verbose=1, shuffle=True)
 
 # 5. 모델 평가하기
-loss_and_metrics = model.evaluate(z_test, z_label, batch_size=32)
+loss_and_metrics = model.evaluate(test_data, test_label, verbose=0)
 print('')
 print('loss_and_metrics : ' + str(loss_and_metrics))
 
 # 6. 모델 사용하기
-xhat_idx = np.random.choice(z_test.shape[0], 5)
-xhat = z_test[xhat_idx]
+xhat_idx = np.random.choice(test_data.shape[0], 5)
+xhat = test_data[xhat_idx]
 yhat = model.predict_classes(xhat)
 
 for i in range(5):
-    print('True : ' + str(np.argmax(z_label[xhat_idx[i]])) + ', Predict : ' + str(yhat[i]))
+    print('True : ' + str(np.argmax(test_label[xhat_idx[i]])) + ', Predict : ' + str(yhat[i]))
--- rename.py
+++ rename.py
@@ -1,12 +1,23 @@
 import os
+import cv2
 
 # 포켓몬 폴더 경로
-file_path = 'D:\\Gongmo\\It_worked\\img\\8\\Purin\\'
+file_path = 'D:\\Gongmo\\It_worked\\img\\9\\Pikachu_pp\\'
 file_names = os.listdir(file_path)
 
 os.chdir(file_path)
-i = 1
-for name in file_names:
-    new_name = str(i) + '.jpg'
-    os.rename(name, new_name)
-    i += 1
+
+def resize_save():
+    for i, name in enumerate(file_names):
+        src = cv2.imread(file_path + name, cv2.IMREAD_GRAYSCALE)
+        resized_img = cv2.resize(src, dsize=(200, 200), fx=1, fy=1, interpolation=cv2.INTER_LINEAR)
+        cv2.imwrite(name, resized_img)
+
+def rename():
+    for i, name in enumerate(file_names):
+        new_name = str(i) + '.jpg'
+        os.renames(name, new_name)
+
+if __name__ == "__main__":
+    resize_save()
+    rename()
